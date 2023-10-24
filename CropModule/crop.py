f63b5d549a696407c057862c40a102eb89e9d19f
lorenspaez <54387407+lorenspaez@users.noreply.github.com>
2023-10-24 00:41:20 -0300

    crop de imagen en la linea

---
+++ CropModule/crop.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+
+
+def recortar_imagen(imagen, bounding_box):
+    x, y, w, h = bounding_box
+    imagen_recortada = imagen[y : y + h, x : x + w]
+    return imagen_recortada
+
+
+def enmascarar_imagen(imagen, bounding_box):
+    x, y, w, h = bounding_box
+    mascara = np.zeros(imagen.shape[:2], dtype=np.uint8)
+    mascara[y : y + h, x : x + w] = 255
+    imagen_enmascarada = cv2.bitwise_and(imagen, imagen, mask=mascara)
+    return imagen_enmascarada
+
+
+def enmascarar_con_fondo_blanco(imagen, bounding_box):
+    x, y, w, h = bounding_box
+    imagen_enmascarada = np.ones_like(imagen) * 255
+    imagen_enmascarada[y : y + h, x : x + w] = imagen[y : y + h, x : x + w]
+    return imagen_enmascarada
+
+
+# Ejemplo CROP:
+imagen = cv2.imread("test.png")
+bounding_box = (300, 0, 500, 650)
+imagen_recortada = recortar_imagen(imagen, bounding_box)
+cv2.imwrite("imagen_recortada.png", imagen_recortada)
+
+
+# Ejemplo MASCARA NEGRA:
+imagen = cv2.imread("test.png")
+bounding_box = (300, 0, 500, 650)
+imagen_enmascarada = enmascarar_imagen(imagen, bounding_box)
+cv2.imwrite("imagen_enmascarada.png", imagen_enmascarada)
+
+
+# Ejemplo MASCARA BLANCA:
+imagen = cv2.imread("test.png")
+bounding_box = (300, 0, 500, 650)
+imagen_blanca = enmascarar_con_fondo_blanco(imagen, bounding_box)
+cv2.imwrite("imagen_enmascarada_blanca.png", imagen_blanca)
